3982fabb4895b90f35a618503744aca00b1d5b54
Iwan Cilibur <32497273+iwancilibur@users.noreply.github.com>
2020-06-21 21:34:47 +0700

    Create blynksend.ino

---
+++ blynksend.ino
@@ -0,0 +1,40 @@
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "masukan tokennya";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "www.interactiverobotics.club";
+char pass[] = "cilibur2019";
+
+BlynkTimer timer;
+
+void kirimdata()
+{
+  float suhu=random(27,35);
+  float kelembaban=random(70,85);
+  
+  Blynk.virtualWrite(V0, suhu);
+  Blynk.virtualWrite(V1, kelembaban);
+  Blynk.virtualWrite(V2, "@iwan cilibur");
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  // You can also specify server:
+  Blynk.begin(auth, ssid, pass);
+  //Blynk.begin(auth, ssid, pass, "iwancilibur.ip-dynamic.com", 8080);
+  timer.setInterval(1000L, kirimdata);
+ }
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
